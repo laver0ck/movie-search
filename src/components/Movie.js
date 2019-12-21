6ff80400eb6cee6d82ae1e1ccd6c44afebc2b59b
laverock <they.all.alike@gmail.com>
2019-12-22 00:35:27 +0300

    add useReducer; cleanup

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DEFAULT_PLACEHOLDER_IMAGE =
-  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+  "https://www.vuecinemas.nl/thumb?w=268&f=jpg&src=userfiles/file/KLER_Poster_World.jpg&alt=img/movie_placeholder.png";
 
 const Movie = ({movie}) => {
   const poster = movie.Poster === "N/A"
